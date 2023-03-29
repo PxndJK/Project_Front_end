d5f8b6fdeb1abe263d64648264f217c04f7a4a9b
danaikritkla <danaikritkla@gmail.com>
2023-03-29 22:27:55 +0700

    edit authen.js

---
+++ control/authen.js
@@ -0,0 +1,22 @@
+const Session = require("../model/listitem").User;
+
+
+module.exports.authentication = async (req, res, next) => {
+    const userId = req.session.userId;
+    if (!userId) {
+        return res.redirect('/login?q=session-expired');
+    }
+    try {
+        const user = await Session.findById(req.session.userId);
+
+        if (!user) {
+            return res.redirect('/login?q=session-expired');
+        }
+
+        next();
+
+    } catch (err) {
+        console.log(err);
+        res.json({ msg: 'Server error. Please reload page after sometime' })
+    }
+};
