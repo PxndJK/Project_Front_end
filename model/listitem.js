f49dda988e67a300babd66b6c35d4497dbe585ca
danaikritkla <danaikritkla@gmail.com>
2023-03-29 22:27:29 +0700

    first commit authen.js

--- model/listitem.js
+++ model/listitem.js
@@ -9,46 +9,46 @@ const UserSchema = new Schema({
     address: String
 });
 
-const foodSchema = new Schema({
-    _id: Number,
-    name: String,
-    description: String,
-    price: Number,
-    image_url: String,
-    type: String
-});
+// const foodSchema = new Schema({
+//     _id: Number,
+//     name: String,
+//     description: String,
+//     price: Number,
+//     image_url: String,
+//     type: String
+// });
 
-const CartSchema = new Schema({
-    _id: Number,
-    user_id: Number,
-    items: [
-        {
-          food_item_id: Number,
-          quantity: Number
-        }
-    ]
-});
+// const CartSchema = new Schema({
+//     _id: Number,
+//     user_id: Number,
+//     items: [
+//         {
+//           food_item_id: Number,
+//           quantity: Number
+//         }
+//     ]
+// });
 
-const OrderSchema = new Schema({
-    _id: Number,
-    user_id: Number,
-    items: [
-        {
-            food_item_id: Number,
-            quantity: Number
-        }
-    ],
-    total_amount: Number,
-    delivery_address: String,
-    created_at: Date,
-    status: String
-});
+// const OrderSchema = new Schema({
+//     _id: Number,
+//     user_id: Number,
+//     items: [
+//         {
+//             food_item_id: Number,
+//             quantity: Number
+//         }
+//     ],
+//     total_amount: Number,
+//     delivery_address: String,
+//     created_at: Date,
+//     status: String
+// });
 
 
 exports.User = mongoose.model("User", UserSchema);
 
-exports.Food = mongoose.model("food", foodSchema);
+// exports.Food = mongoose.model("food", foodSchema);
 
-exports.Cart = mongoose.model("Cart", CartSchema);
+// exports.Cart = mongoose.model("Cart", CartSchema);
 
-exports.Order = mongoose.model("Order", OrderSchema);
+// exports.Order = mongoose.model("Order", OrderSchema);
