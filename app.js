f43b113409327d8369ac3f23f13eb2972c582993
danaikritkla <danaikritkla@gmail.com>
2023-04-01 21:51:05 +0700

    edit app.js

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
 const express = require("express");
 const app = express();
+const expressValidator = require('express-validator');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const flash = require('connect-flash');
+const path = require('path');
 const mongoose = require("mongoose");
 const listUser = require("./model/listitem").User;
 const listAdmin = require("./model/listitem").Admin;
-// const listFood = require("./model/listitem").Food;
-// const listCart = require("./model/listitem").Cart;
-// const listOrder = require("./model/listitem").Order;
-const session = require("express-session")
+const listFood = require("./model/listitem").Food;
+const listCart = require("./model/listitem").Cart;
+const listOrder = require("./model/listitem").Order;
+const session = require("express-session");
 const at = require("./control/authen")
 const atadmin = require("./control/authenad")
+var config = require('./config/database');
+var request = require('request');
+var fileUpload = require('express-fileupload')
 
-// Configure session middleware
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false
-}));
 
-app.use(flash());
 
-mongoose.connect('mongodb://127.0.0.1:27017/Projectfontend')
+mongoose.connect(config.database)
 .then(()=> console.log('Database is connected'))
 .catch((e) => console.log(e));
 
 app.use(express.static(__dirname + "/public"));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
+
+
+//Get Page Model
+var Page = require('./model/page');
+
+//Express fileUpload middleware
+app.use(fileUpload());
+
+//Body Parser middleware
+//
+//parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+//parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(session({
     secret: "jklfsodifjsktnwjasdp465dd", // Never ever share this secret in production, keep this in separate file on environmental variable
@@ -39,25 +52,80 @@ app.use(session({
     mongoUrl : ({mongoUrl: "mongodb://127.0.0.1:27017/Projectfontend"}),
 }));
 
+//Express Validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root    = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  },
+  customValidators: {
+    isImage: function(value, filename){
+      var extension = (path.extname(filename)).toLowerCase();
+      switch(extension) {
+        case '.jpg':
+            return '.jpg';
+        case '.jpeg':
+            return '.jpeg';
+        case '.png':
+            return '.png';
+        case '':
+            return '.jpg';
+        default:
+            return false;
+      }
+    }
+  }
+}));
+
+app.get('*', function(req, res,next) {
+    res.locals.cart = req.session.cart;
+    next();
+})
+
+// Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+
 app.get("/" ,(req,res) => {
     res.render("Home")
 })
 
-app.get("/" ,(req,res) => {
-    res.render("cartnew")
-})
+// app.get("/" ,(req,res) => {
+//     res.render("cartnew")
+// })
 
 app.get("/Home" ,(req,res) => {
     res.render("Home")
 })
 
+app.get("/Checkoutss" ,(req,res) => {
+    res.render("Checkoutss")
+})
+
+app.get("/Tracking" ,(req,res) => {
+    res.render("Tracking")
+})
 
 app.get('/HomeUser', at.authentication, async (req, res) => {
     try {
       const user = await listUser.findById(req.session.userId);
       if (user) {
         const username = user.Username;
-        res.render('HomeUser', { Username: username }); //push data on ejs
+        res.render('HomeUser', { Username: username });
       } else {
         res.redirect('/login'); 
       }
@@ -70,31 +138,31 @@ app.get('/HomeUser', at.authentication, async (req, res) => {
 
   
 
-app.get("/cartnew" ,(req,res) => {
-    res.render("cartnew")
-})
+// app.get("/cartnew" ,(req,res) => {
+//     res.render("cartnew")
+// })
 
-app.get('/Menu', at.authentication, async (req, res) => {
-    try {
-      const user = await listUser.findById(req.session.userId);
-      if (user) {
-        const username = user.Username;
-        res.render('Menu', { Username: username });
-      } else {
-        res.redirect('/login'); 
-      }
-    } catch (error) {
-      console.error('Error retrieving user:', error);
-      res.status(500).send('Internal Server Error');
-    }
-});
+// app.get('/Menu', at.authentication, async (req, res) => {
+//     try {
+//       const user = await listUser.findById(req.session.userId);
+//       if (user) {
+//         const username = user.Username;
+//         res.render('Menu', { Username: username });
+//       } else {
+//         res.redirect('/login'); 
+//       }
+//     } catch (error) {
+//       console.error('Error retrieving user:', error);
+//       res.status(500).send('Internal Server Error');
+//     }
+// });
 
 app.get("/login" ,(req,res) => {
-  res.render('login',{message: req.flash('message')})
+    res.render("login")
 })
 
 app.get("/loginadmin" ,(req,res) => {
-  res.render('loginadmin',{message: req.flash('message')})
+    res.render("loginadmin")
 })
 
 app.post('/loginadmin', async (req,res)=>{
@@ -106,7 +174,6 @@ app.post('/loginadmin', async (req,res)=>{
         console.log(req.session);
         res.redirect('/admin');
        } else {
-        req.flash('message','Email or password is incorrect. please try again');
         res.redirect('/loginadmin')
     }
 })
@@ -120,7 +187,6 @@ app.post('/login', async (req,res)=>{
         console.log(req.session);
         res.redirect('/HomeUser');
        } else {
-        req.flash('message','Email or password is incorrect. please try again');
         res.redirect('/login')
     }
 })
@@ -128,6 +194,10 @@ app.post('/login', async (req,res)=>{
 app.get("/register" ,(req,res) => {
     res.render("register")
 })
+
+// app.get("/products" ,(req,res) => {
+//   res.render("all")
+// })
 
 app.post('/register', async(req,res)=>{
     // User
@@ -176,19 +246,35 @@ app.get("/admin" , atadmin.authenticationadmin, async (req,res) => {
       }
 })
 
-app.get("/Checkout" ,(req,res) => {
-    res.render("Checkout")
-})
 
-app.get("/Tracking" ,(req,res) => {
-    res.render("Tracking")
-})
+
+
+
+var products = require('./routes/products.js');
+// var adminPages = require('./routes/admin_pages.js');
+var cart = require('./routes/cart.js');
+var adminCategories = require('./routes/admin_categories.js');
+var adminProducts = require('./routes/admin_products.js');
+
+// app.use('/admin/pages', adminPages);
+app.use('/admin/categories', adminCategories);
+app.use('/admin/products', adminProducts);
+app.use('/cart', cart);
+
+app.use('/products', products);
+
+
+
 
 app.post('/logout',(req,res)=>{
     req.session.destroy(function (err) {
         res.redirect('/'); 
     });
 })
+
+//set routes
+
+
 
 app.listen(3000, function () {
     console.log("Server app listening on port");
